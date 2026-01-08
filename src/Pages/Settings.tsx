00297a7e8bdb42c2916bb494c7632631b59839cf
Ankit <ankitahuja80@gmail.com>
2026-01-09 01:22:29 +0530

    Delete an account functionality added

--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -1,8 +1,22 @@
 import { useNavigate } from "react-router-dom";
+import type { AppDispatch } from "../redux/store/store";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/actions/authAction";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 
 type SettingItem = {
   label: string;
   path: string;
+  type?: string;
 };
 
 const accountSettings: SettingItem[] = [
@@ -20,11 +34,44 @@ const legalSettings: SettingItem[] = [
   { label: "Refund & Cancellation Policy", path: "/refund-policy" },
 ];
 
+const accountActions: SettingItem[] = [
+  { label: "Logout", path: "", type: "logout" },
+  { label: "Delete Account", path: "", type: "deleteAccount" },
+];
+
 const Settings = () => {
+  const [isDeleteDialogOpen, setisDeleteDialogOpen] = useState(false);
+
   const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  const handleDeleteAccount = () => {
+    handleOpen();
+  };
+
+  const handleOpen = () => {
+    setisDeleteDialogOpen(true);
+  };
+
+  const handleClose = () => {
+    setisDeleteDialogOpen(false);
+  };
+
+  const handleDelete = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const formJson = Object.fromEntries((formData as any).entries());
+    const email = formJson.email;
+    console.log(email);
+    handleClose();
+  };
 
   const renderSection = (title: string, items: SettingItem[]) => (
-    <section className="mb-10 ">
+    <section className="mb-7 ">
       <h2 className="mb-4 text-sm font-semibold tracking-wide text-gray-500 uppercase">
         {title}
       </h2>
@@ -33,13 +80,23 @@ const Settings = () => {
         {items.map((item, index) => (
           <button
             key={item.label}
-            onClick={() => navigate(item.path)}
+            onClick={() =>
+              item.type === "logout"
+                ? handleLogout()
+                : item.type === "deleteAccount"
+                ? handleDeleteAccount()
+                : navigate(item.path)
+            }
             className={`w-full flex items-center justify-between px-6 py-4 text-left
               hover:bg-gray-50 transition cursor-pointer
               ${index !== items.length - 1 ? "border-b border-gray-200" : ""}
             `}
           >
-            <span className="text-base font-medium text-gray-800">
+            <span
+              className={`text-base font-medium ${
+                item.type === "deleteAccount" ? "text-red-500" : "text-gray-800"
+              } `}
+            >
               {item.label}
             </span>
 
@@ -52,15 +109,46 @@ const Settings = () => {
 
   return (
     <div className="max-w-5xl mx-auto px-8 py-8">
-      <header className="mb-5">
-        {/* <h1 className="text-3xl font-semibold text-gray-900">Settings</h1> */}
-        {/* <p className="mt-2 text-sm text-gray-500">
-          Manage your account preferences and legal information
-        </p> */}
-      </header>
-
       {renderSection("Account Settings", accountSettings)}
       {renderSection("Information & Legal", legalSettings)}
+      {renderSection("Account Actions", accountActions)}
+
+      <Dialog open={isDeleteDialogOpen} onClose={handleClose}>
+        <DialogTitle className="font-semibold text-red-600">
+          Delete Account Permanently
+        </DialogTitle>
+
+        <DialogContent>
+          <DialogContentText>
+            This action cannot be undone. Deleting your account will permanently
+            remove all your data. Please confirm your identity by entering your
+            password below.
+          </DialogContentText>
+
+          <form onSubmit={handleDelete} id="delete-account-form">
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="password"
+              name="password"
+              label="Confirm Password"
+              type="password"
+              fullWidth
+              variant="standard"
+            />
+          </form>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={handleClose} color="inherit">
+            Cancel
+          </Button>
+          <Button type="submit" form="delete-account-form" color="error">
+            Delete Account
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
