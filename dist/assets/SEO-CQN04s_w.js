a6a4379e95102d8bb0f857529ad16ef6892c52e3
Ankit <ankitahuja80@gmail.com>
2026-03-02 03:02:02 +0530

    Updated build

---
+++ dist/assets/SEO-CQN04s_w.js
@@ -0,0 +1,1 @@
+import{j as t,R as n}from"./index-DoB9B6Hq.js";const i=({title:e,description:s})=>t.jsxs(n,{children:[t.jsx("title",{children:e}),t.jsx("meta",{name:"description",content:s})]});export{i as S};
--- dist/assets/SEO-DSwC01U2.js
+++
@@ -1,1 +0,0 @@
-import{j as t,R as n}from"./index-BMUkZNs7.js";const i=({title:e,description:s})=>t.jsxs(n,{children:[t.jsx("title",{children:e}),t.jsx("meta",{name:"description",content:s})]});export{i as S};
